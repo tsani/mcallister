184a2abbdf7bd30c38002ffa919821f1c6663b88
Jacob Errington <git@mail.jerrington.me>
2017-05-05 15:10:26 -0400

    style

--- frontend/index.js
+++ frontend/index.js
@@ -8,16 +8,26 @@
       getStatus(
         function(response) {
           var s = '';
+          var d = null;
           if(response.status) {
             s = 'green';
             msg = 'Someone is home :)';
+            d = function(e) {
+              e.removeClass('black');
+              e.addClass('white');
+            }
           }
           else {
             s = 'red';
             msg = 'Nobody is home :(';
+            d = function(e) {
+              e.addClass('black');
+              e.removeClass('white');
+            }
           }
           $('body').css('background-color', s);
           status.text(msg);
+          d(status);
         },
         function(errors) {
           console.log("uh oh! " + JSON.stringify(errors));
